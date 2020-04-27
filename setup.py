68578dd887f2e0f62c1e8bf95323574cfc489be0
Sushant Nadkar <ssn.andymax@gmail.com>
2020-04-27 13:05:19 +0530

    version(patch)-incremented

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as f:
 
 setup(
 	name = "vcgencmd",
-	version = "0.1.0",
+	version = "0.1.1",
 	description = "Python binding for RaspberryPi vcgencmd command-line tool",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
